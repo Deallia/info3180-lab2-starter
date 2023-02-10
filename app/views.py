96d612ede1d837b1a2b83ef26ef60d3b56fa37b1
Deallia Dunbar <dealliadunbar@gmail.com>
2023-02-10 01:47:48 -0500

    added profile.html and wrote format_date_joined function

--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
 ###
+
 
 @app.route('/')
 def home():
@@ -17,6 +18,17 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile')
+def profile():
+    """Render website's profile page."""
+    return render_template('profile.html', date=format_date_joined())
+
+
+
+def format_date_joined():
+    
+    date_joined =  datetime.date(2023, 2, 9)
+    return ("" + date_joined.strftime("%B, %Y"))
 
 ###
 # The functions below should be applicable to all Flask apps.
